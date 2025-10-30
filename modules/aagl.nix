552954c8df3675f15d2d37969504964d87c3a331
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-10-29 22:05:01 -0500

    Add configuration for multiple launcher programs

---
+++ modules/aagl.nix
@@ -0,0 +1,11 @@
+{ inputs, ... }: {
+  imports = [ inputs.aagl.nixosModules.default ];
+
+  nix.settings = inputs.aagl.nixConfig;
+
+  programs = {
+    anime-game-launcher.enable = true;
+    honkers-railway-launcher.enable = true;
+    sleepy-launcher.enable = true;
+  };
+}
