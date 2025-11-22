bc38c501769a01c597f8ef8a108af1db5f5a7223
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-22 18:16:16 -0500

    fix: move track-layout outside of xkb

--- modules/system/programs/niri/home.nix
+++ modules/system/programs/niri/home.nix
@@ -21,13 +21,13 @@
           xkb = {
             layout = "us,es";
             options = "grp:alt_shift_toggle";
-            track-layout = "window";
           };
         };
         mouse = {
           accel-speed = -0.2;
           accel-profile = "flat";
         };
+        track-layout = "window";
       };
 
       outputs = {
