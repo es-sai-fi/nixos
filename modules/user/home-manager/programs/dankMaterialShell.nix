0527146650b0552b007608299e50c324ecef2a93
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-01 17:23:28 -0500

    dms: define session defaults

--- modules/user/home-manager/programs/dankMaterialShell.nix
+++ modules/user/home-manager/programs/dankMaterialShell.nix
@@ -5,18 +5,34 @@
 
   programs.dankMaterialShell = {
     enable = true;
-    systemd.enable = true;
+
+    systemd = {
+      enable = true;
+      restartIfChanged = true;
+    };
+
     enableSystemMonitoring = true;
     enableClipboard = true;
     enableCalendarEvents = true;
     enableSystemSound = true;
-    default.settings = {
-      theme = "dark";
-      showDock = true;
-      dockAutoHide = true;
-      clockDateFormat = "yyyy-MM-dd";
-      weatherEnabled = false;
-      dankBarSquareCorners = true;
+
+    default = {
+      session = {
+        isLightMode = false;
+        nightModeEnabled = true;
+        nightModeTemperature = 3000;
+        nightModeHighTemperature = 3500;
+        nightModeAutoEnabled = false;
+      };
+      settings = {
+        theme = "dark";
+        showDock = true;
+        dockAutoHide = true;
+        clockDateFormat = "yyyy-MM-dd";
+        weatherEnabled = false;
+        dankBarSquareCorners = true;
+        nightModeEnabled = true;
+      };
     };
   };
 }
