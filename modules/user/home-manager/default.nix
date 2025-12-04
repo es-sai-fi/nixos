628097d05b2b28e109e3f8ddc35eafc879734c39
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-03 22:06:28 -0500

    home-manager: set neovide as def editor

--- modules/user/home-manager/default.nix
+++ modules/user/home-manager/default.nix
@@ -40,6 +40,7 @@
       sessionVariables = {
         NIXOS_OZONE_WL = "1";
         REGISTRY_AUTH_FILE = "${config.home.homeDirectory}/.config/containers/auth.json";
+        EDITOR = "neovide";
         FZF_DEFAULT_COMMAND = "fd --hidden --strip-cwd-prefix --exclude .git";
         FZF_CTRL_T_COMMAND = "$FZF_DEFAULT_COMMAND";
         FZF_ALT_C_COMMAND = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
