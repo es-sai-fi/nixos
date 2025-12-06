e7c4700f0d8c3ea8559bdce09776c762c6a47c94
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-06 01:09:46 -0500

    move env variables and packages to system config

--- modules/system/programs/default.nix
+++ modules/system/programs/default.nix
@@ -1,15 +1,43 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   imports = [
     ./niri
     ./aagl.nix
   ];
 
-  environment.systemPackages = with pkgs; [
-    vim
-    git
-    wget
-    curl
-  ];
+  environment = {
+    sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+      REGISTRY_AUTH_FILE = "${config.home.homeDirectory}/.config/containers/auth.json";
+      FZF_DEFAULT_COMMAND = "fd --hidden --strip-cwd-prefix --exclude .git";
+      FZF_CTRL_T_COMMAND = "$FZF_DEFAULT_COMMAND";
+      FZF_ALT_C_COMMAND = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
+    };
+    systemPackages = with pkgs; [
+      curl
+      git
+      openssh
+      wget
+
+      bleachbit
+      lm_sensors
+      tealdeer
+      unzip
+      xdg-utils
+      zip
+
+      ffmpeg-full
+      obs-cmd
+      obs-studio
+
+      discord
+
+      minizinc
+    ];
+  };
 
   programs = {
     dconf.enable = true;
--- modules/user/home-manager/default.nix
+++ modules/user/home-manager/default.nix
@@ -23,29 +23,10 @@
     };
 
     home = {
-      packages = with pkgs; [
-        lm_sensors
-        zip
-        unzip
-        openssh
-        ffmpeg-full
-        obs-studio
-        bleachbit
-        discord
-        obs-cmd
-        xdg-utils
-        minizinc
-      ];
-      sessionVariables = {
-        NIXOS_OZONE_WL = "1";
-        REGISTRY_AUTH_FILE = "${config.home.homeDirectory}/.config/containers/auth.json";
-        FZF_DEFAULT_COMMAND = "fd --hidden --strip-cwd-prefix --exclude .git";
-        FZF_CTRL_T_COMMAND = "$FZF_DEFAULT_COMMAND";
-        FZF_ALT_C_COMMAND = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
-      };
       stateVersion = "25.05";
       shell.enableFishIntegration = true;
     };
+
     dconf.settings = {
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
