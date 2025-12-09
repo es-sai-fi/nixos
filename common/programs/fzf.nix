9fb56235a67bffe15a34aa6c1b26341bad95af0e
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-08 23:58:08 -0500

    refactor: fzf

---
+++ common/programs/fzf.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: {
+  environment = {
+    systemPackages = [pkgs.fzf];
+    envVariables = {
+      FZF_DEFAULT_COMMAND = "fd --type f --color always --exclude .git --ignore-file ~/.gitignore";
+      FZF_CTRL_T_COMMAND = "$FZF_DEFAULT_COMMAND";
+      FZF_ALT_C_COMMAND = "fd --type d --color always --exclude .git --ignore-file ~/.gitignore";
+    };
+  };
+}
