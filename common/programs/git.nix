33642775f33e0105ae68b08fbed0a49e6bc515d9
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-10 15:11:36 -0500

    feat: wrap git

--- common/programs/git.nix
+++ common/programs/git.nix
@@ -1,20 +1,31 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  gitIni = pkgs.formats.gitIni {};
+  gitConfig = gitIni.generate "git-config" {
+    init.defaultBranch = "main";
+    pull.rebase = false;
+    push.autoSetupRemote = true;
+    user = {
+      email = "96452903+es-sai-fi@users.noreply.github.com";
+      name = "es-sai-fi";
+    };
+  };
+  git-wrapped = pkgs.symlinkJoin {
+    name = "git-wrapped";
+    paths = [
+      pkgs.git
+    ];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      mkdir -p $out/git
+      cp ${gitConfig} $out/git/config
+
+      wrapProgram $out/bin/hx \
+        --set XDG_CONFIG_HOME $out
+    '';
+  };
+in {
   programs.git = {
     enable = true;
-  };
-
-  hjem.users.es-sai-fi.xdg.config.files."git/config" = let
-    gitIni = pkgs.formats.gitIni {};
-    settings = {
-      init.defaultBranch = "main";
-      pull.rebase = false;
-      push.autoSetupRemote = true;
-      user = {
-        email = "96452903+es-sai-fi@users.noreply.github.com";
-        name = "es-sai-fi";
-      };
-    };
-  in {
-    source = gitIni.generate "git-config" settings;
+    package = git-wrapped;
   };
 }
