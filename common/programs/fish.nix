ff1df0484435e7f36836e63bd5235e9083b0ba5a
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-11 11:17:46 -0500

    feat: fish.nix

---
+++ common/programs/fish.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  programs.fish = {
+    enable = true;
+    package = import ../wrappers/fish {inherit pkgs;};
+    useBabelFish = true;
+    interactiveShellInit = ''
+      set fish_greeting
+    '';
+    shellAliases = {
+      cd = "z";
+      cat = "bat";
+    };
+  };
+}
