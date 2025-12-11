385f284153c26af438e339a25a65474e392ebc12
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-11 13:12:21 -0500

    feat: wrap bottom

---
+++ common/wrappers/bottom.nix
@@ -0,0 +1,10 @@
+{pkgs}:
+pkgs.symlinkJoin {
+  name = "bottom-wrapped";
+  paths = [pkgs.bottom];
+  buildInputs = [pkgs.makeWrapper];
+  postBuild = ''
+    wrapProgram $out/bin/btm \
+      --add-flag "--theme gruvbox"
+  '';
+}
