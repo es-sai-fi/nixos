64dabfc8abbf714055f4f696399534cbf10eab27
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-11 16:30:52 -0500

    feat: wrap bat

--- common/programs/bat.nix
+++ common/programs/bat.nix
@@ -1,3 +1,12 @@
-{
-  programs.bat.enable = true;
+{pkgs, ...}: let
+  batWrapped = import ../wrappers/bat.nix {inherit pkgs;};
+in {
+  environment.variables = {
+    PAGER = "bat";
+  };
+
+  programs.bat = {
+    enable = true;
+    package = batWrapped;
+  };
 }
---
+++ common/wrappers/bat.nix
@@ -0,0 +1,11 @@
+{pkgs}:
+pkgs.symlinkJoin {
+  name = "bat-wrapped";
+  paths = [pkgs.bat];
+  buildInputs = [pkgs.makeWrapper];
+  postBuild = ''
+    wrapProgram $out/bin/bat \
+      --set BAT_THEME gruvbox-dark \
+      --set BAT_PAGER builtin
+  '';
+}
