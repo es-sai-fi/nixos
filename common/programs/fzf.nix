bb71709aa488e84af68eaedc7cdc3d8182416323
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-11 20:56:07 -0500

    feat: wrap fzf

--- common/programs/fzf.nix
+++ common/programs/fzf.nix
@@ -1,15 +1,5 @@
-{pkgs, ...}: {
-  environment = {
-    systemPackages = [pkgs.fzf];
-    variables = {
-      FZF_DEFAULT_COMMAND = "fd --type f --color always --exclude .git --ignore-file ~/.gitignore";
-      FZF_CTRL_T_COMMAND = "$FZF_DEFAULT_COMMAND";
-      FZF_ALT_C_COMMAND = "fd --type d --color always --exclude .git --ignore-file ~/.gitignore";
-    };
-  };
-
-  programs.fzf = {
-    fuzzyCompletion = true;
-    keybindings = true;
-  };
+{pkgs, ...}: let
+  fzfWrapped = import ../wrappers/fzf.nix {inherit pkgs;};
+in {
+  environment.systemPackages = [fzfWrapped];
 }
---
+++ common/wrappers/fzf.nix
@@ -0,0 +1,12 @@
+{pkgs}:
+pkgs.symlinkJoin {
+  name = "fzf-wrapped";
+  paths = [pkgs.fzf];
+  buildInputs = [pkgs.makeWrapper];
+  postBuild = ''
+    wrapProgram $out/bin/fzf \
+      --set FZF_DEFAULT_COMMAND "fd --type f --color always --exclude .git --ignore-file ~/.gitignore" \
+      --set FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND" \
+      --set FZF_ALT_C_COMMAND "fd --type d --color always --exclude .git --ignore-file ~/.gitignore"
+  '';
+}
