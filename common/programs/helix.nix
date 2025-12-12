52c89256ad93f7042b36565eca924a61157a246f
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-12 17:04:01 -0500

    fix: nixConfig -> nix.settings

--- common/programs/helix.nix
+++ common/programs/helix.nix
@@ -9,7 +9,7 @@
     helixPackage = helix.packages.${system}.default;
   };
 in {
-  nixConfig = {
+  nix.settings = {
     extra-substituters = ["https://helix.cachix.org"];
     extra-trusted-public-keys = [
       "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
