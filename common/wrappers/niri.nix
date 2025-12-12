fe86340a28abfe7547c2d28235d1c5363b31fcda
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-12 17:42:41 -0500

    fix: remove builtins.readFile idk why i used that

--- common/wrappers/niri.nix
+++ common/wrappers/niri.nix
@@ -1,15 +1,13 @@
-{pkgs}: let
-  niriConfigFile = builtins.readFile "./config.kdl";
-in
-  pkgs.symlinkJoin {
-    name = "niri-wrapped";
-    paths = [pkgs.niri];
-    buildInputs = [pkgs.makeWrapper];
-    postBuild = ''
-      mkdir $out/niri
-      cp ${niriConfigFile} $out/niri/config.kdl
+{pkgs}:
+pkgs.symlinkJoin {
+  name = "niri-wrapped";
+  paths = [pkgs.niri];
+  buildInputs = [pkgs.makeWrapper];
+  postBuild = ''
+    mkdir $out/niri
+    cp ${./config.kdl} $out/niri/config.kdl
 
-      wrapProgram $out/bin/niri \
-        --set NIRI_CONFIG $out/niri
-    '';
-  }
+    wrapProgram $out/bin/niri \
+      --set NIRI_CONFIG $out/niri
+  '';
+}
