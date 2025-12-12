a32b62fb4b3d43606f68557bc18a6dd7157130bd
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-12 17:03:10 -0500

    fix: manually place cachix config for helix

--- common/programs/helix.nix
+++ common/programs/helix.nix
@@ -9,8 +9,12 @@
     helixPackage = helix.packages.${system}.default;
   };
 in {
-  nix.settings = helix.nixConfig;
-
+  nixConfig = {
+    extra-substituters = ["https://helix.cachix.org"];
+    extra-trusted-public-keys = [
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+    ];
+  };
   environment = {
     systemPackages = [helixWrapped];
     sessionVariables.EDITOR = "hx";
