a338f538dad0493a41cafe0878b482fb911c6082
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-12-12 23:22:51 -0500

    feat: remove ~/.gitignore ref from fzf

--- common/wrappers/fzf.nix
+++ common/wrappers/fzf.nix
@@ -5,9 +5,9 @@ pkgs.symlinkJoin {
   buildInputs = [pkgs.makeWrapper];
   postBuild = ''
     wrapProgram $out/bin/fzf \
-      --set FZF_DEFAULT_COMMAND "fd --type f --color always --exclude .git --ignore-file ~/.gitignore" \
+      --set FZF_DEFAULT_COMMAND "fd --type f --color always --exclude .git" \
       --set FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND" \
-      --set FZF_ALT_C_COMMAND "fd --type d --color always --exclude .git --ignore-file ~/.gitignore"
+      --set FZF_ALT_C_COMMAND "fd --type d --color always --exclude .git"
   '';
   meta.mainProgram = "fzf";
 }
